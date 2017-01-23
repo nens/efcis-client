49611d66b4c73a56bb2df56fe4f5eeaffbd7b9b1
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-01-23 16:33:15 +0100

    Removed chunked loading for now

--- index.jsx
+++ index.jsx
@@ -8,6 +8,10 @@ import { Provider } from 'react-redux';
 import configureStore from './configureStore.jsx';
 import * as localForage from 'localforage';
 
+import TableApp from './components/TableApp.jsx';
+import MapApp from './components/MapApp.jsx';
+import ChartApp from './components/ChartApp.jsx';
+
 const store = configureStore();
 const persistor = persistStore(store, { storage: localForage });
 
@@ -34,14 +38,14 @@ class App extends Component {
     return (
        <Provider store={store} persistor={persistor}>
          <Router history={hashHistory}>
-           <Route path='/' component={
-             require('react-router?name=[name]!./components/TableApp.jsx')
+           <Route path='/' component={TableApp
+            //  require('react-router?name=[name]!./components/TableApp.jsx')
            } />
-           <Route path='map' component={
-             require('react-router?name=[name]!./components/MapApp.jsx')
+           <Route path='map' component={MapApp
+            //  require('react-router?name=[name]!./components/MapApp.jsx')
            } />
-           <Route path='chart' component={
-             require('react-router?name=[name]!./components/ChartApp.jsx')
+           <Route path='chart' component={ChartApp
+            //  require('react-router?name=[name]!./components/ChartApp.jsx')
            } />
          </Router>
        </Provider>
